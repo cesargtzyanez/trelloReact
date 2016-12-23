f0c7139bb78467979795f6dd51edb9f24e9cf525
César Gutiérrez Yáñez <cesargy@gmail.com>
2016-12-22 23:17:39 -0600

    Some fixes done

--- src/Column.js
+++ src/Column.js
@@ -4,7 +4,7 @@
 
 import React, { Component } from 'react';
 import './Column.css';
-import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import {Card, CardTitle} from 'material-ui/Card';
 import FlatButton from 'material-ui/FlatButton';
 import {List, ListItem} from 'material-ui/List';
 import TextField from 'material-ui/TextField';
@@ -20,17 +20,18 @@ class Column extends Component{
     };
     this.addItemHandler = this.addItemHandler.bind(this);
     this.textChangeHandler = this.textChangeHandler.bind(this);
+    this.deleteItemHandler = this.deleteItemHandler.bind(this);
   }
 
   render(){
     return(
         <Card className="column">
-          <CardHeader title={this.props.title} subtitle={this.state.numChildren + ' items'}/>
+          <CardTitle title={this.props.title} subtitle={this.state.numChildren + ' items'}/>
             <List>{this.state.itemsList}</List>
             <TextField
               ref="newItemTextField"
-              hintText="New Todo Item"
-              floatingLabelText="Add a new Todo Item"
+              hintText={"New " + this.props.title + " item"}
+              floatingLabelText={"Add a new " + this.props.title + " Item"}
               onChange={this.textChangeHandler}
             />
             <FlatButton label="Add..." primary={true} onClick={this.addItemHandler}/>
@@ -39,8 +40,8 @@ class Column extends Component{
   }
 
   addItemHandler = function () {
-    if(this.state.newItem != ''){
-      this.state.itemsList.push(<ListItem className="listItem" rightIcon={<ActionDelete />} key={this.state.numChildren} primaryText={this.state.newItem} />);
+    if(this.state.newItem !== ''){
+      this.state.itemsList.push(<ListItem className="listItem" rightIcon={<ActionDelete key={'item_'+this.state.numChildren} onClick={this.deleteItemHandler} />} key={'item'+this.state.numChildren} primaryText={this.state.newItem} />);
       this.setState(state => ({newItem: ''}));
       this.refs.newItemTextField.getInputNode().value='';
       this.setState(state => ({numChildren: state.numChildren + 1}));
@@ -49,6 +50,10 @@ class Column extends Component{
 
   textChangeHandler = function () {
     this.setState(state => ({newItem: this.refs.newItemTextField.getValue()}));
+  }
+
+  deleteItemHandler = function (e) {
+
   }
 }
 export default Column;
